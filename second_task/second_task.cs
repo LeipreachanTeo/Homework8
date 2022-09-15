0ddd67140e0b65a03e45690766c28e115ed63d21
LeipreachanTeo <pokiboom@mail.ru>
2022-09-15 21:39:08 +0700

    Second task

---
+++ second_task/second_task.cs
@@ -0,0 +1,51 @@
+﻿int lines= 2;
+int colums = 2;
+int[,] two_dim_array_one = new int [ lines, colums ];
+int[,] two_dim_array_two = new int [lines, colums];
+
+void FillTwoDimArrayRDM(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(10);
+        }
+    }   
+}
+
+void PrintTwoDimArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j]+ " ");
+        }
+    Console.WriteLine();
+    }
+}
+void ProductMartix(int[,]martix_one, int[,] matrix_two)
+{
+    int [,] matrix_product = new int [martix_one.GetLength(0), matrix_two.GetLength(1)];
+    for (int i = 0; i < martix_one.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix_two.GetLength(0); j++)
+        {
+            for(int k = 0; k < matrix_two.GetLength(1); k++)
+            {
+                matrix_product[i,j] += martix_one[i,k] * matrix_two[k,j]; 
+            }
+        Console.Write(matrix_product[i,j]+ " ");   
+        }
+    Console.WriteLine();
+    }
+}
+FillTwoDimArrayRDM(two_dim_array_one);
+FillTwoDimArrayRDM(two_dim_array_two);
+Console.WriteLine($"\nПервая матрица:");
+PrintTwoDimArray(two_dim_array_one);
+Console.WriteLine($"Вторая матрица:");
+PrintTwoDimArray(two_dim_array_two);
+Console.WriteLine($"Произведение матриц:");
+ProductMartix(two_dim_array_one, two_dim_array_two);
